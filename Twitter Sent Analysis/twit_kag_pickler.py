6d85d40c39c578c122bc1f1db1699af54a36cb22
Alec <alec.mcgregor@strath.ac.uk>
2019-06-11 12:00:30 +0100

    pickles
    added pickles

---
+++ Twitter Sent Analysis/twit_kag_pickler.py
@@ -0,0 +1,84 @@
+# DataFrame
+import pandas as pd
+
+# Matplot
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+# Scikit-learn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+from sklearn.manifold import TSNE
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Keras
+from Keras.preprocessing.text import Tokenizer
+from Keras.preprocessing.sequence import pad_sequences
+from Keras.models import Sequential
+from Keras.layers import Activation, Dense, Dropout, Embedding, Flatten, Conv1D, MaxPooling1D, LSTM
+from Keras import utils
+from Keras.callbacks import ReduceLROnPlateau, EarlyStopping
+
+# nltk
+import nltk
+from nltk.corpus import stopwords
+from  nltk.stem import SnowballStemmer
+
+# Word2vec
+import gensim
+
+# Utility
+import re
+import numpy as np
+import os
+from collections import Counter
+import logging
+import time
+import pickle
+import itertools
+
+# Set log
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+# SETTINGS
+
+# DATASET
+DATASET_COLUMNS = ["target", "ids", "date", "flag", "user", "text"]
+DATASET_ENCODING = "ISO-8859-1"
+TRAIN_SIZE = 0.8
+
+# TEXT CLENAING
+TEXT_CLEANING_RE = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
+
+# WORD2VEC 
+W2V_SIZE = 300
+W2V_WINDOW = 7
+W2V_EPOCH = 32
+W2V_MIN_COUNT = 10
+
+# KERAS
+SEQUENCE_LENGTH = 300
+EPOCHS = 8
+BATCH_SIZE = 1024
+
+# SENTIMENT
+POSITIVE = "POSITIVE"
+NEGATIVE = "NEGATIVE"
+NEUTRAL = "NEUTRAL"
+SENTIMENT_THRESHOLDS = (0.4, 0.7)
+
+# EXPORT
+KERAS_MODEL = "model.h5"
+WORD2VEC_MODEL = "model.w2v"
+TOKENIZER_MODEL = "tokenizer.pkl"
+ENCODER_MODEL = "encoder.pkl"
+
+# READ DATA
+
+
+dataset_path = "Data Sets/kaggleset.csv"
+print("Open file:", dataset_path)
+df = pd.read_csv(dataset_path, encoding=DATASET_ENCODING, names=DATASET_COLUMNS)
+
+df.head(5)
